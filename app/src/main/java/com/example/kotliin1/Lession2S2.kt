b3a75001b542562c52c351112c34bef218e2acfc
jatin-bhuva <jatin.bhuva@tatvasoft.com>
2025-01-16 13:18:21 +0530

    Added grid layout

--- app/src/main/java/com/example/kotliin1/Lession2S2.kt
+++ app/src/main/java/com/example/kotliin1/Lession2S2.kt
@@ -2,17 +2,18 @@ package com.example.kotliin1
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
+import android.widget.ArrayAdapter
 import android.widget.Button
-import android.widget.TextView
+import android.widget.GridView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class Lession2S2 : AppCompatActivity() {
-    private lateinit var activityCycleLog: TextView
+    private lateinit var gridView: GridView
     private lateinit var shareBtn: Button
+    private lateinit var adapter: ArrayAdapter<String>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -22,13 +23,19 @@ class Lession2S2 : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        activityCycleLog = findViewById(R.id.activtyCycleLog)
         shareBtn = findViewById(R.id.share_button)
         val logText = intent.getStringExtra("lifecycleLog")
-        activityCycleLog.text = logText
         shareBtn.setOnClickListener{
             shareLogText(logText.toString())
         }
+
+        val logEntries = logText?.split("\n") ?: emptyList()
+
+        gridView = findViewById(R.id.gridViewLog)
+
+        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_activated_1, logEntries)
+
+        gridView.adapter = adapter
     }
 
     private fun shareLogText(logText: String) {
